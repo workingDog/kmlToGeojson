b2ad0ab6b19999e821370fd90cbfd7a3fdd8f8e5
workingDog <ringowathelet@gmail.com>
2015-11-01 13:30:42 +0900

    initial commit

--- src/main/scala/com/kodekutters/Converter.scala
+++ src/main/scala/com/kodekutters/Converter.scala
@@ -1,20 +1,18 @@
 package com.kodekutters
 
-import com.scalakml.io.KmlFileReader
-import com.scalakml.kml._
-import com.scalakml.{kml => KML}
+import java.io.{IOException, File, PrintWriter}
+
+import com.scalakml.io.{KmzFileReader, KmlFileReader}
 import play.api.libs.json.Json
-
-import scala.collection.immutable.Seq
-
+import play.extras.geojson.{GeoJson, LatLng}
 
 /**
   * convert a Kml file into a GeoJSON representation
   *
   * @author R. Wathelet
   *
-  * ref: https://developers.google.com/kml/documentation/kmlreference
-  * ref: http://geojson.org/geojson-spec.html
+  *         ref: https://developers.google.com/kml/documentation/kmlreference
+  *         ref: http://geojson.org/geojson-spec.html
   */
 object Converter {
 
@@ -25,27 +23,47 @@ object Converter {
     if (args.length == 0)
       println(usage)
     else {
-      if (args.nonEmpty) {
-        if (args(0).toLowerCase.endsWith("kml")) kmlToGeoJson(args(0))
-        if (args(0).toLowerCase.endsWith("kmz")) kmzToGeoJson(args(0))
-      }
+      // the output
+      val outFile = if (args.length == 2) args(1) else ""
+      // the input
+      if (args(0).toLowerCase.endsWith("kml")) kmlToGeoJson(args(0), outFile)
+      if (args(0).toLowerCase.endsWith("kmz")) kmzToGeoJson(args(0), outFile)
     }
   }
 
-  def kmlToGeoJson(fileName: String) = {
-    val kml = new KmlFileReader().getKmlFromFile(fileName)
+  def kmlToGeoJson(inFile: String, outFile: String) = {
+    val kml = new KmlFileReader().getKmlFromFile(inFile)
     val geojson = KmlConverter().toGeoJson(kml)
-    geojson.foreach(obj => println("geojson obj: \n" + obj))
-    //  println("\n")
-    //  geojson.foreach(obj => println("geojson json: \n" + Json.toJson(obj)))
+    if (outFile.isEmpty) {
+      geojson.foreach(obj => println(obj))  // testing
+      geojson.foreach(obj => println(Json.prettyPrint(Json.toJson(obj))))
+    } else {
+      writeToFile(outFile, Seq(geojson))
+    }
   }
 
-  def kmzToGeoJson(fileName: String) = {
-    val kml = new KmlFileReader().getKmlFromFile(fileName)
-    val geojson = KmlConverter().toGeoJson(kml)
-    geojson.foreach(obj => println("geojson obj: \n" + obj))
-    //  println("\n")
-    //  geojson.foreach(obj => println("geojson json: \n" + Json.toJson(obj)))
+  def kmzToGeoJson(inFile: String, outFile: String) = {
+    val kmlSeq = new KmzFileReader().getKmlFromKmzFile(inFile)
+    // convert each kml file to GeoJson
+    val geojsonSeq = for (kml <- kmlSeq) yield KmlConverter().toGeoJson(kml)
+    if (outFile.isEmpty) {
+      geojsonSeq.foreach(obj => println(obj)) // testing
+      geojsonSeq.foreach(obj => println(Json.prettyPrint(Json.toJson(obj))))
+    } else {
+      writeToFile(outFile, geojsonSeq)
+    }
+  }
+
+  private def writeToFile(outFile: String, geojsonSeq: Seq[Option[List[GeoJson[LatLng]]]]) = {
+    val writer = new PrintWriter(new File(outFile))
+    try {
+      geojsonSeq.foreach(obj => writer.write(Json.prettyPrint(Json.toJson(obj))))
+    } catch {
+      case e: IOException => e.printStackTrace()
+    }
+    finally {
+      writer.close()
+    }
   }
 
 }
