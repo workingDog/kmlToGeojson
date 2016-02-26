56600d345c36720990d3a8b46e5c2f9a29356180
workingDog <ringowathelet@gmail.com>
2016-02-26 19:19:52 +0900

    minor update

--- src/main/scala/com/kodekutters/LatLngAlt.scala
+++ src/main/scala/com/kodekutters/LatLngAlt.scala
@@ -13,7 +13,9 @@ import scala.collection.immutable.Seq
   * @param alt The altitude in meters, default relative to the WGS84 ellipsoid,
   *            but can be interpreted according to the AltitudeMode of a Feature
   */
-case class LatLngAlt(lat: Double, lng: Double, alt: Option[Double])
+case class LatLngAlt(lat: Double, lng: Double, alt: Option[Double] = None) {
+  def this(lat: Double, lng: Double, alt: Double) = this(lat, lng, Option(alt))
+}
 
 object LatLngAlt {
   implicit val latLngAltFormat: Format[LatLngAlt] = Wgs84Format.format
@@ -22,13 +24,14 @@ object LatLngAlt {
 
 /**
   * The WGS84 CRS format. Equals to EPSG:4326 CRS format.
+  * for GeoJSON read and write (lng,lat,alt) not (lat,lng,alt)
   */
 object Wgs84Format extends CrsFormat[LatLngAlt] {
   val crs = NamedCrs("urn:ogc:def:crs:OGC:1.3:CRS84")
   val format = Format[LatLngAlt](
     __.read[Seq[Double]].map {
           case Seq(lng, lat, alt) => LatLngAlt(lat, lng, Some(alt))
-          case Seq(lng, lat) => LatLngAlt(lat, lng, None)
+          case Seq(lng, lat) => LatLngAlt(lat, lng)
     },
     Writes(latLngAlt => {
       latLngAlt.alt match {
